51b5463fbf4c73c85c4269f13c96f87b2e13c723
Noranosan <nsmrym04@gmail.com>
2024-11-12 22:58:34 +0900

    add:コンポーネント作成、ts対応

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState } from "react";
+
+import Title from "./components/Title";
+import Form from "./components/Form";
+import Results from "./components/Results";
 import './App.css';
 
+type ResultsStateType = {
+  country: string;
+  cityName: string;
+  temperature: string;
+  conditionText: string;
+  icon: string;
+}
+
 function App() {
+  const [city, setCity] = useState<string>("");
+  const [results, setResults] = useState<ResultsStateType>({
+    country: "",
+    cityName: "",
+    temperature: "",
+    conditionText: "",
+    icon: ""
+  });
+  const getWeather = (e:any) => {
+      e.preventDefault();
+      fetch("https://api.weatherapi.com/v1/current.json?key=929a4275ed0c4275982160943230704&q=London&aqi=no")
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title />
+      <Form setCity={setCity} getWeather={getWeather} />
+      <Results />
     </div>
   );
 }
